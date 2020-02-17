ce9ca0c71972cbd17a6201bfb1dbc516c6f33398
Mathew Polzin <matt.polzin@gmail.com>
2020-02-17 15:53:50 -0800

    bump OpenAPIKit version.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "openapi-diff", targets: ["openapi-diff"])
     ],
     dependencies: [
-        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .upToNextMinor(from: "0.17.0")),
+        .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .upToNextMinor(from: "0.18.0")),
         .package(url: "https://github.com/mattpolzin/OrderedDictionary.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/mattpolzin/Poly.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/jpsim/Yams.git", .upToNextMajor(from: "2.0.0"))
