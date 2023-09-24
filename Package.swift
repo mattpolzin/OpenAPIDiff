8b7de92a08e289dfde8a9efe38d74a317e828b85
Mathew Polzin <matt.polzin@gmail.com>
2023-09-24 18:16:01 -0500

    bump argument parser version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/mattpolzin/OpenAPIKit.git", .branch("release/3_0")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.3.0")),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
         .package(url: "https://github.com/fabianfett/pure-swift-json.git", .upToNextMinor(from: "0.4.0"))
     ],
     targets: [
